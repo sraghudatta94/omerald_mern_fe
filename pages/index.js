1bffdcb3e4d3fe53d0f2f955448ef9fb498af81a
saifmohammed888 <saifmohammed888@gmail.com>
2022-03-19 11:51:07 +0530

    Initial template

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Omerald</title>
+                <meta name="description" content="Find Articles for better health" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>
+                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                </h1>
+            </main>
+        </div>
+    )
+}
