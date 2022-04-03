ac2cd4a5466e5bd92963652a452b1880ff07f0bd
saifmohammed888 <msaif1996>
2022-04-03 11:41:55 +0530

    Register refactored

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,14 @@
-import { Links } from '@components/atoms/link';
-import { Layout } from '@components/common';
 import { articles, authors, topics } from 'prisma/db/getData';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import React from 'react';
+import dynamic from 'next/dynamic';
 import articleActionCreator from 'redux/actions/article';
 import authorActionCreator from 'redux/actions/author';
 import topicActionCreator from 'redux/actions/topics';
+import { Links } from '@components/atoms/link';
+
+const Layout = dynamic(() => import('@components/common'));
 
 const Register = ({ article, author, topic }) => {
   const dispatch = useDispatch();
@@ -24,42 +27,38 @@ const Register = ({ article, author, topic }) => {
     <Layout>
       <main className="bg-grey pt-80 pb-50">
         <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-xl-5 col-md-10">
-              <div className="login_wrap widget-taber-content p-30 h-[35vh] bg-white border-radius-10">
-                <div className="padding_eight_all bg-white">
-                  <div className="heading_s1 text-center">
-                    <h3 className="mb-30 text-2xl  font-weight-9900">
-                      Create an account
-                    </h3>
-                  </div>
-                  <form method="post">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        className="form-control"
-                        name="contact"
-                        placeholder="Enter Contact"
-                      />
-                    </div>
-
-                    <div className="form-group  flex justify-center">
-                      <button
-                        type="submit"
-                        className="button w-[15vw] bg-blue-700 button-contactForm btn-block"
-                      >
-                        Send &amp; OTP
-                      </button>
-                    </div>
-                  </form>
-
-                  <div className="text-muted text-center">
-                    Already have an account?
-                    <Links href="/login">
-                      <a> Sign in now</a>
-                    </Links>
-                  </div>
+          <div className="login_wrap widget-taber-content w-[40vw] m-auto p-30 h-[45vh] bg-white border-radius-10">
+            <div className="padding_eight_all bg-white">
+              <div className="heading_s1 text-center">
+                <h3 className="mb-30 text-2xl  font-weight-9900">
+                  Create an account
+                </h3>
+              </div>
+              <form method="post">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="contact"
+                    placeholder="Enter Contact"
+                  />
                 </div>
+
+                <div className="form-group  flex justify-center">
+                  <button
+                    type="submit"
+                    className="button w-[15vw] bg-blue-700 button-contactForm btn-block"
+                  >
+                    Send &amp; OTP
+                  </button>
+                </div>
+              </form>
+
+              <div className="text-muted text-center">
+                Already have an account?
+                <Links href="/login">
+                  <a> Sign in now</a>
+                </Links>
               </div>
             </div>
           </div>
